d6d0b5f2cf5f9d10508e13bae7d3428dec4461e5
Samira <samira.salimian@zoodfood.com>
2018-02-16 01:36:44 +0330

    homepage-v1

--- js/project.js
+++ js/project.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
 
-// All JS for website goes here
+	// Slider
+	var slider = new MasterSlider();
+	slider.setup('masterslider' , {
+	  width:1920,
+	  height:580,
+	  loop:true,
+	  autoplay:true,
+	  fullwidth:true,
+	  overPause:false,
+	  centerControls:false,
+	  speed:16,
+	  view:'fade'
+	});
+	slider.control('bullets' , {
+		autohide:false  , 
+		// dir:"h", 
+		// align:"bottom"
+	});
 
+	// //Product Slider
+	// (function() {
+	// 	document.documentElement.className = 'js';
+	// 	var slideshow = new CircleSlideshow(document.getElementById('slideshow'));
+	// })();
 });
