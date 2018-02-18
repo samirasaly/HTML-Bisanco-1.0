94a5619183cf65856a8ed3f11584799b95525f62
Samira <samira.salimian@zoodfood.com>
2018-02-18 18:16:28 +0330

    bugfix

--- js/project.js
+++ js/project.js
@@ -38,16 +38,66 @@ $(document).ready(function(){
 		prevEffect : 'none',
 		nextEffect : 'none',
 
-		closeBtn  : false,
-		arrows    : false,
 		nextClick : true,
 
 		helpers : {
 			thumbs : {
 				width  : 50,
 				height : 50
-			}
+			},
+			title : {
+						type : 'inside'
+					},
+					buttons	: {}
 		}
 	});
+
+	// Header Scroll
+	$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 200) {
+        //clearHeader, not clearheader - caps H
+        $(".navigation").addClass("sticky");
+    }
+    if (scroll < 200) {
+        //clearHeader, not clearheader - caps H
+        $(".navigation").removeClass("sticky");
+    }
+}); //missing );
+
+});
+
+// Form Validate
+
+$('.contact-form').validate({
+  rules: {
+    full_name: {
+      required: true,
+      minlength: 3
+    },
+    subject: {
+      required: true,
+      minlength: 3
+    },
+    email: {
+      required: true,
+      minlength: 5,
+      email:true
+    },
+    phone: {
+      required: true,
+      minlength: 5,
+      number:true
+    }
+  },
+  errorPlacement: function(error, element) {
+    return false;
+    alert('error');
+  },
+  submitHandler: function (form) {
+    $('.thank-you').addClass('active');        
+  }
 
 });
